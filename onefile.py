7aba6c922565571de937240a8a45c986737fda9b
malecaut <117046969+malecaut@users.noreply.github.com>
2024-07-05 20:48:17 +0100

    session added as class member

--- onefile.py
+++ onefile.py
@@ -3,35 +3,37 @@ from bs4 import BeautifulSoup
 
 class OnefileClass:
 
+    def __init__(self):
+        self.session = requests.session()
+
     def login(self,uname,pword):
-        with requests.session() as s:
-            r = s.get("https://onefile.co.uk/")
-            file = open("Results_GetOnefile.txt",'w')
-            file.write(r.text)
-            file.close()
+        r = self.session.get("https://onefile.co.uk/")
+        file = open("Results_GetOnefile.txt",'w')
+        file.write(r.text)
+        file.close()
 
-            r = s.get('https://login.onefile.co.uk/')
-            file = open("Results_GetLogin.txt",'w')
-            file.write(r.text)
-            file.close()
-            for cookie in r.cookies:
-                print(cookie)
+        r = self.session.get('https://login.onefile.co.uk/')
+        file = open("Results_GetLogin.txt",'w')
+        file.write(r.text)
+        file.close()
+        for cookie in r.cookies:
+            print(cookie)
 
-            payload = {
-                'email' : uname,
-                'password' : pword
-            }
+        payload = {
+            'email' : uname,
+            'password' : pword
+        }
 
-            r = s.post('https://login.onefile.co.uk/api/authentication',json=payload)
+        r = self.session.post('https://login.onefile.co.uk/api/authentication',json=payload)
 
-            file = open("Results_PostLogin.txt",'w')
-            file.write(r.text)
-            file.close()
-            print(r.status_code)
+        file = open("Results_PostLogin.txt",'w')
+        file.write(r.text)
+        file.close()
+        print(r.status_code)
 
-            r = s.get('https://login.onefile.co.uk/api/user')
-            file = open("Results_GetUser.txt",'w')
-            file.write(r.text)
-            file.close()
-            print(r.status_code)
+        r = self.session.get('https://login.onefile.co.uk/api/user')
+        file = open("Results_GetUser.txt",'w')
+        file.write(r.text)
+        file.close()
+        print(r.status_code)
 
