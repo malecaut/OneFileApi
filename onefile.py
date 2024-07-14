47593c788f999450419b851bb395bf331d3fef09
malecaut <117046969+malecaut@users.noreply.github.com>
2024-07-14 02:07:58 +0100

    refactored get method to journal approach

--- onefile.py
+++ onefile.py
@@ -5,20 +5,29 @@ import json
 class OneFileTimesheetEntry:
 
     def __init__(self):
-        self.categoryLabel = str()
+        #self.categoryLabel = str()
+
         self.dateFromLabel = str()
         self.dateToLabel = str()
+        self.timeFromLabel = str()
+        self.timeToLabel = str()
+        self.dateCreated = str()
+        self.timeCreated = str()
+
         self.titleLabel = str()
         self.timeLabel = str()
-        self.createdOnLabel = str()
+
+        #self.createdOnLabel = str()
         self.timesheetId = str()
         self.isOTJ = str()
         self.timeSheetCategoryId = str()
+
         self.textData = str()
         self.trainingActivities = list()
         self.criteria = list()
         self.files = list()
         self.linkedActivityId = str()
+        
 
 
 
@@ -30,6 +39,7 @@ class OnefileClass:
 
         self.session = requests.session()
         self.entries = list()
+        self.entryCounter = int()
 
     def login(self,uname,pword):
         
@@ -96,6 +106,7 @@ class OnefileClass:
             print(cookie)
         
         file = open("Results7_GetTimesheets.txt","r")
+        self.entryCounter= 0 
         for line in file:
             if 'CategoryLabel' in line:
                 entry = OneFileTimesheetEntry()
@@ -110,6 +121,7 @@ class OnefileClass:
                     elif 'TimeLabel' in line:
                         entry.timeLabel = line.split('>')[1].split('<')[0]
                         self.entries.append(entry)
+                        self.entryCounter = self.entryCounter + 1
                         break
         file.close()
         file = open("StructureLog.txt",'w')
@@ -128,80 +140,105 @@ class OnefileClass:
         file.close()
         data = json.loads(r.text)
         for journalJson in data:
-            for entry in self.entries:
-                if entry.titleLabel == journalJson['comments']:
-                    entry.timesheetId = str(journalJson['timesheetID'])
-                    entry.isOTJ = str(journalJson['isOffTheJob'])
-                    entry.timeSheetCategoryId = str(journalJson['timeSheetCategoryID'])
-                    entry.createdOnLabel = str(journalJson['dateTimesheet'][0:16])
+            entry = OneFileTimesheetEntry()
+            entry.timesheetId = str(journalJson['timesheetID'])
+            entry.dateFromLabel = journalJson['dateFrom'][0:9]
+            entry.timeFromLabel = journalJson['dateFrom'].split('T')[1][0:7]
+            entry.dateToLabel = journalJson['dateTo'][0:9]
+            entry.timeToLabel = journalJson['dateTo'].split('T')[1][0:7]
+            entry.dateCreated = journalJson['dateTimesheet'][0:9]
+            entry.timeCreated = journalJson['dateTimesheet'].split('T')[1][0:7]
+            entry.titleLabel = journalJson['comments']
+            entry.timeLabel = str(journalJson['time'])
+            entry.isOTJ = str(journalJson['isOffTheJob'])
+            entry.timeSheetCategoryId = str(journalJson['timeSheetCategoryID'])
+            self.entries.append(entry)
+
+            #entry.createdOnLabel = str(journalJson['dateTimesheet'][0:16])
+            #for entry in self.entries:
+                #if entry.titleLabel == journalJson['comments']:
+                    #entry.timesheetId = str(journalJson['timesheetID'])
+                    #entry.isOTJ = str(journalJson['isOffTheJob'])
+                    #entry.timeSheetCategoryId = str(journalJson['timeSheetCategoryID'])
+                    #entry.createdOnLabel = str(journalJson['dateTimesheet'][0:16])
+                
         
-        file = open("StructureLog.txt",'w')
-        for entry in self.entries:
-            file.write('    ' + entry.categoryLabel + '\n')
-            file.write(entry.dateFromLabel + '\n')
-            file.write(entry.dateToLabel + '\n')
-            file.write(entry.titleLabel + '\n')
-            file.write(entry.timeLabel + '\n')
-            file.write(entry.timesheetId + '\n')
-            file.write(entry.isOTJ + '\n')
-            file.write(entry.timeSheetCategoryId + '\n')
-            file.write(entry.createdOnLabel + '\n')
-        file.close()
+        #file = open("StructureLog.txt",'w')
+        #for entry in self.entries:
+            #file.write('    ' + entry.categoryLabel + '\n')
+            #file.write(entry.dateFromLabel + '\n')
+            #file.write(entry.dateToLabel + '\n')
+            #file.write(entry.titleLabel + '\n')
+            #file.write(entry.timeLabel + '\n')
+            #file.write(entry.timesheetId + '\n')
+            #file.write(entry.isOTJ + '\n')
+            #file.write(entry.timeSheetCategoryId + '\n')
+            #file.write(entry.createdOnLabel + '\n')
+        #file.close()
 
         r = self.session.get("https://learning.onefile.co.uk/api/journalEntry")
         file = open("Results9_GetJournalDetailed.txt",'w')
         file.write(r.text)
-        file.close()
+        #file.close()
 
         data = json.loads(r.text)
-        for journalJson in data:
-            for entry in self.entries:
-                if entry.createdOnLabel == journalJson['createdOn'][0:16]:
-                    entry.textData = journalJson['entryData']['text']
-                    if entry.textData == "":
-                        entry.textData = "You added no comments"
-                    #entry.trainingActivities = str(journalJson['entryData']['trainingActivities'])
-                    for activity in journalJson['entryData']['trainingActivities']:
-                        entry.trainingActivities.append(str(activity))
-                    #entry.trainingActivities = [str(activityId) for activityId in journalJson['entryData']['trainingActivities']]
-                    for criteria in journalJson['entryData']['criteria']:
-                        entry.criteria.append(criteria)
-                    #entry.criteria = str(journalJson['entryData']['criteria'])
-                    #entry.files = str(journalJson['entryData']['files'])
-                    #if journalJson['entryData']['files']:
-                        #entry.files = [str(file_id) for file_id in journalJson['entryData']['files']]
-                    #else:
-                        #entry.files.append("")
-                    if journalJson['entryData']['files']:
-                        for afile in journalJson['entryData']['files']:
-                            entry.files.append(str(afile))
-                        entry.linkedActivityId = str(journalJson['linkedActivityId'])
-                    #for activity in journalJson['linkedActivityId']:
-                        #entry.linkedActivityId.append(activity)
-                    #entry.linkedActivityId = [str(activityId) for activityId in journalJson['linkedActivityId']]
+        lastDate = str()
 
-                    break
-                                        
+        counter = 0
+        while data:
+            for journalJson in data:
+                lastDate = str(journalJson['createdOn'])[0:19]
+                found = False
+                for entry in self.entries:
+                    #if entry.createdOnLabel == journalJson['createdOn'][0:16]:
+                    if entry.timesheetId == str(journalJson['linkedActivityId']).strip():
+                    #if entry.timesheetId == str(journalJson['entryData']['trainingActivities'][0]):
+                        counter = counter + 1
+                        found = True
+                        entry.textData = journalJson['entryData']['text']
+                        if entry.textData == "":
+                            entry.textData = "You added no comments"
+                        for activity in journalJson['entryData']['trainingActivities']:
+                            entry.trainingActivities.append(str(activity))
+                        if journalJson['entryData']['criteria']:
+                            for criteria in journalJson['entryData']['criteria']:
+                                entry.criteria.append(criteria)
+                        if journalJson['entryData']['files']:
+                            for afile in journalJson['entryData']['files']:
+                                entry.files.append(str(afile))
+                        entry.linkedActivityId = str(journalJson['linkedActivityId']).strip()
+                        break
+                if found == False:
+                    print(str(journalJson['entryData']['trainingActivities'][0]))
+            rParams = {
+                'lastEntryDate' : lastDate
+            }
+            #r = self.session.get("https://learning.onefile.co.uk/api/journalEntry",json=rParams)
+            r = self.session.get("https://learning.onefile.co.uk/api/journalEntry?lastEntryDate=" + lastDate)
+            file.write(r.text)
+
+            print(r.status_code)
+            print(lastDate)
+            data = json.loads(r.text)
+        file.close()
+        print(counter)    
+        print(self.entryCounter)                      
 
         file = open("StructureLog.txt",'w')
         for entry in self.entries:
-            file.write('\n\t\t' + entry.categoryLabel + '\n')
-            file.write("Date from: " + entry.dateFromLabel + '\n')
+            #file.write('\n\t\t' + entry.categoryLabel + '\n')
+            file.write('\n' + "Date from: " + entry.dateFromLabel + '\n')
             file.write("Date to: " + entry.dateToLabel + '\n')
             file.write("Title: " + entry.titleLabel + '\n')
             file.write("Time: " + entry.timeLabel + '\n')
             file.write("Timesheet id: " + entry.timesheetId + '\n')
             file.write("OTJ: " + entry.isOTJ + '\n')
             file.write("Timesheet category id: " + entry.timeSheetCategoryId + '\n')
-            file.write("Date created: " + entry.createdOnLabel + '\n')
+            #file.write("Date created: " + entry.createdOnLabel + '\n')
             file.write("Description: " + entry.textData + '\n')
-            #for line in entry.trainingActivities:
-                #file.write(line + '\n')
-            #file.write("Training activity: " + entry.trainingActivities + '\n')
             file.write("Training Activities: " + '\n')
             for activity in entry.trainingActivities:
                 file.write('\t' + activity + '\n')
-            #file.write(entry.criteria)
             file.write("Criteria: " + '\n')
             for criteria in entry.criteria:
                 file.write('\t' + str(criteria) + '\n')
